e7cecd4adcea4cb7ce85c23903cdfbbdf9497e0d
javier maestro <javiermaestrogarcia@gmail.com>
2019-07-24 15:22:04 +0200

    primer commit en la primeraRama

---
+++ primera rama/codigo2.c
@@ -0,0 +1,9 @@
+nt main(int argc, char *argv[]){
+	char kk;
+	int T; //Tests a resoldre
+	int i, X, Y;
+	FILE *fin, *fout;
+	
+	printf("Solució guardada al fitxer fitxer_sortida.txt!\n" );
+	return 0;
+}
